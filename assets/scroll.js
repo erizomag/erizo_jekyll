0bc8aa618ae37e72401477c6089e27ca0d12b01b
Lisa Sy <lisasyis@gmail.com>
2018-07-19 21:18:54 -0700

    Update navigation and display of desktop experience
    - Disable scrolling on `body` on art sample
    - Enable left-right button navigation on art sample
    - Update certain breakpoints to `desktop` instead of `tablet`
    - Begin working on mobile experience
    - Add small transition elements

--- assets/scroll.js
+++ assets/scroll.js
@@ -10,10 +10,11 @@ $(function () {
     $("html, body").animate({
       scrollTop: $(".container").offset().top},
       1000, function() {
+        $(".sample_container").addClass("hidden");
     });
 
     $(".container").addClass("active");
-    $(".sample_slideshow_nav").removeClass("active");
+    $(".sample_slideshow_bottom_nav").removeClass("active");
   });
 
 
@@ -23,9 +24,53 @@ $(function () {
     $(".sample_container").animate({
       scrollLeft: "+=" + panelWidth},
       1000, function() {
-        $(".sample_slideshow_nav").addClass("active");
+        $(".sample_slideshow_bottom_nav").addClass("active");
+        $(".sample_slideshow_right_nav").addClass("active");
+        $(".sample_slideshow_left_nav").addClass("active");
+        $(".cta_nav_to_slideshow").addClass("hidden");
     });
 
     $("body").addClass("default_splash");
+    $(".sample_container").addClass("active");
+  });
+
+  $(".sample_slideshow_left_nav").click(function(e){
+    e.preventDefault();
+
+    $(".sample_container").animate({
+      scrollLeft: "-=" + panelWidth},
+      1000, function() {
+    });
+  });
+
+  $(".sample_slideshow_right_nav").click(function(e){
+    e.preventDefault();
+
+    $(".sample_container").animate({
+      scrollLeft: "+=" + panelWidth},
+      1000, function() {
+    });
+  });
+
+  $(".sample_container").scroll(function(){
+      var $elem = $(".sample_container");
+      var newScrollLeft = $elem.scrollLeft(),
+          width = $elem.width(),
+          scrollWidth = $elem.get(0).scrollWidth;
+      var offset = 0;
+
+      if (scrollWidth - newScrollLeft - width === offset) {
+        // alert('right end')
+        $(".sample_slideshow_right_nav").removeClass("active");
+        $(".sample_slideshow_bottom_nav").addClass("emphasized");
+      }
+
+      if (newScrollLeft === 0) {
+        $(".sample_container").removeClass("active");
+        // alert('left')
+        $(".sample_slideshow_right_nav").removeClass("active");
+        $(".sample_slideshow_left_nav").removeClass("active");
+        $(".sample_slideshow_bottom_nav").removeClass("active");
+      }
   });
 });
